69db3e46688ab695ffb9e0b0e29c0ede6b7d478d
Sander van Harmelen <sander@vanharmelen.nl>
2019-07-25 16:18:48 +0200

    Add outputs to return the access key and secret

--- main.tf
+++ main.tf
@@ -1,26 +1,28 @@
-resource "aws_iam_user" "default" {
-  name = "${var.name}-Account"
+resource aws_iam_user default {
+  name = "${var.name}Account"
 }
 
-resource "aws_iam_access_key" "default" {
-  user = "${aws_iam_user.default.name}"
+resource aws_iam_access_key default {
+  user = aws_iam_user.default.name
 }
 
-resource "aws_iam_user_policy" "default" {
-  name   = "${var.name}-Policy"
-  user   = "${aws_iam_user.default.name}"
+resource aws_iam_user_policy default {
+  name   = "${var.name}Policy"
+  user   = aws_iam_user.default.name
   policy = var.policy
 }
 
-resource "aws_ssm_parameter" "access_key_id" {
-  name   = "/${lower(var.name)}/credentials/access_key_id"
+resource aws_ssm_parameter access_key_id {
+  count  = var.use_ssm ? 1 : 0
+  name   = "/${lower(var.name)}account/credentials/access_key_id"
   type   = "SecureString"
   value  = aws_iam_access_key.default.id
   key_id = var.kms_key_id
 }
 
-resource "aws_ssm_parameter" "secret_access_key" {
-  name   = "/${lower(var.name)}/credentials/secret_access_key"
+resource aws_ssm_parameter secret_access_key {
+  count  = var.use_ssm ? 1 : 0
+  name   = "/${lower(var.name)}account/credentials/secret_access_key"
   type   = "SecureString"
   value  = aws_iam_access_key.default.secret
   key_id = var.kms_key_id
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,19 @@
-output "ssm_access_key_id" {
-  value       = aws_ssm_parameter.access_key_id.name
+output access_key_id {
+  value       = aws_iam_access_key.default.id
+  description = "The access key ID"
+}
+
+output secret_access_key {
+  value       = aws_iam_access_key.default.secret
+  description = "The secret access key"
+}
+
+output ssm_access_key_id {
+  value       = aws_ssm_parameter.access_key_id[0].name
   description = "The SSM access key ID parameter name"
 }
 
-output "ssm_secret_access_key" {
-  value       = aws_ssm_parameter.secret_access_key.name
+output ssm_secret_access_key {
+  value       = aws_ssm_parameter.secret_access_key[0].name
   description = "The SSM secret access key parameter name"
 }
