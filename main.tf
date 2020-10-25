22680093ae7a93d6a2577f621c4b4019992d0b4a
Marwin Baumann <mbaumann@schubergphilis.com>
2020-10-25 11:32:44 +0100

    append user i.s.o account also for the ssm resources

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_iam_user_policy_attachment" "default" {
 }
 
 resource "aws_ssm_parameter" "access_key_id" {
-  name   = "/${lower(var.name)}account/credentials/access_key_id"
+  name   = "/${lower(var.name)}${var.postfix ? "user" : ""}/credentials/access_key_id"
   type   = "SecureString"
   value  = aws_iam_access_key.default.id
   key_id = var.kms_key_id
@@ -34,7 +34,7 @@ resource "aws_ssm_parameter" "access_key_id" {
 }
 
 resource "aws_ssm_parameter" "secret_access_key" {
-  name   = "/${lower(var.name)}account/credentials/secret_access_key"
+  name   = "/${lower(var.name)}${var.postfix ? "user" : ""}/credentials/secret_access_key"
   type   = "SecureString"
   value  = aws_iam_access_key.default.secret
   key_id = var.kms_key_id
